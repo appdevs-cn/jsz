c6bc2bfa43bd88c837ce6b9e6030ed23c9b44e30
何云龙 <734274314@qq.com>
2017-05-25 03:40:59 +0800

    活动页面

--- Application/Home/Controller/PromotionController.class.php
+++ Application/Home/Controller/PromotionController.class.php
@@ -19,6 +19,30 @@ class PromotionController extends CommonController
         $comment = $category_info['comment'];
         $comment = '<p>' . implode('</p><p>', explode("\n", $comment)) . '</p>';
         $rows = parse_promotion_config($category_info['config']);
+        $level2vip = [
+            '4' => '白银VIP',
+            '5' => '黄金VIP',
+            '6' => '钻石VIP',
+        ];
+        foreach($rows as &$row) {
+            foreach($row as $key => &$value) {
+                switch ($key) {
+                    case 'level':
+                        if($value > 3) {
+                            $value = $level2vip[$value];
+                        }
+                        break;
+                    case 'max_count':
+                        if($value == 0) {
+                            $value = '不封顶';
+                        }
+                        break;
+                    case 'return_rate':
+                        $value = sprintf("%.2f%", $value * 100);
+                        break;
+                }
+            }
+        }
         $keys = config_key_en_2_zn(array_keys($rows[0]));
         $this->assign('title', $promotion['title']);
         $this->assign('subtitle', $promotion['subtitle']);
