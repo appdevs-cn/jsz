f0f95e063f57950ec13c01877c46f7003d38bdc7
何云龙 <734274314@qq.com>
2017-05-25 01:25:05 +0800

    活动页面

--- Application/Home/Controller/PromotionController.class.php
+++ Application/Home/Controller/PromotionController.class.php
@@ -8,4 +8,15 @@ class PromotionController extends CommonController
         $this->menu = "promotion";
         $this->display();
     }
+
+    public function show()
+    {
+        $pid = I('get.pid');
+        $promotion = M('Promotion')->where("id = $pid")->find();
+        $category_info = M('PromotionCategory')->where("id = {$promotion['cid']}")->find();
+        $this->assign('title', $promotion['title']);
+        $this->assign('subtitle', $promotion['subtitle']);
+        $this->assign('description', $category_info['description']);
+        $this->display();
+    }
 }
